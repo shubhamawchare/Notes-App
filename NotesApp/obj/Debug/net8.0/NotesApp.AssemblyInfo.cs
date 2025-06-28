5cd4642ccaf076229441122c5fa45e0b1cb3161d
Shubham Awchare <shubhamawchare@gmail.com>
2025-06-28 15:41:59 +0530

    Readme file

--- NotesApp/obj/Debug/net8.0/NotesApp.AssemblyInfo.cs
+++ NotesApp/obj/Debug/net8.0/NotesApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29406902d578b79524b5e52168f07ea75eb0f6d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
